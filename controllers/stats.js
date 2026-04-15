5d3b5b13fa697fac330fc530991a345b61cd104e
ChantelleO7 <obeirnechantelle@gmail.com>
2026-04-15 13:33:50 +0100

    more stats

--- controllers/stats.js
+++ controllers/stats.js
@@ -13,13 +13,20 @@ const stats = {
     let numSongs = playlists.reduce((total, playlist) => total + playlist.songs.length, 0);
 	
 	  let average = numPlaylists > 0 ? (numSongs / numPlaylists).toFixed(2) : 0;
-
+      let totalRating = playlists.reduce((total, playlist) => total + parseInt(playlist.rating), 0);
+      let avgRating = numPlaylists > 0 ? totalRating/numPlaylists : 0;
+      let maxRating = Math.max(...playlists.map(playlist => playlist.rating));
+      let maxRated = playlists.filter(playlist => playlist.rating === maxRating);
+      let favTitles = maxRated.map(item => item.title);
 
     const statistics = {
-      displayNumPlaylists: numPlaylists,
-      displayNumSongs: numSongs,
-	    displayAverage: average
-    }
+        displayNumPlaylists: numPlaylists,
+        displayNumSongs: numSongs,
+         displayAverage: average,
+        displayAvgRating: avgRating.toFixed(2),
+	    highest: maxRating,
+        displayFav: favTitles
+    };
 
     const viewData = {
       title: "Playlist App Statistics",
